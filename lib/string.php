9380c9a21f22bb2c0648f544b7b2ddef11f44eee
Jin Huang <jin@jin-online.com>
2016-06-08 15:52:55 +0800

    test first commit

---
+++ lib/string.php
@@ -0,0 +1,29 @@
+<?php
+function utf8_wordwrap($string, $length = 20, $break = "\n") {
+    preg_match_all('~\p{Latin}+|\p{Han}+|\p{Hiragana}+|\p{Katakana}+|\p{Bopomofo}+|\p{Common}+~u', $string, $words);
+    $new_words = array();
+    foreach ($words[0] as $word) {
+        if(mb_detect_encoding($word)=='UTF-8' && mb_strlen($word)>1){
+            $ary_word = preg_split('/(?<!^)(?!$)/u', $word);
+            foreach ($ary_word as $chinese_word) {
+                $new_words[] = $chinese_word;
+            }
+        }else{
+            $new_words[] = $word;
+        }
+    }
+
+    $lines = array();
+    $strlen = 0;
+    $ret = '';
+    foreach ($new_words as $n_word) {
+        if($strlen + mb_strwidth($n_word) >= $length){
+            $ret.= $break;
+            $strlen = 0;
+        }
+        $ret .= $n_word;
+        $strlen += mb_strwidth($n_word);
+    }
+
+    return $ret;
+}
